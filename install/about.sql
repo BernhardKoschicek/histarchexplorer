4f050d92ca80710478ad9828b73a747fe6e551d3
lisaldrian <lis.aldrian@gmail.com>
2024-04-30 14:54:34 +0200

    about.sql

---
+++ install/about.sql
@@ -0,0 +1,37 @@
+DROP SCHEMA IF EXISTS tng CASCADE;
+CREATE SCHEMA IF NOT EXISTS tng;
+
+CREATE TABLE IF NOT EXISTS tng.config (
+    id SERIAL PRIMARY KEY,
+  	name TEXT,
+    description TEXT,
+   	address TEXT,
+    config_class TEXT,
+    email TEXT,
+    orcid_id TEXT,
+    image TEXT,
+    website TEXT,
+    language TEXT DEFAULT 'en'
+);
+
+CREATE TABLE IF NOT EXISTS tng.links (
+ 	id SERIAL PRIMARY KEY,
+	domain_id INT,
+	range_id INT,
+	property INT
+	);
+
+CREATE TABLE IF NOT EXISTS tng.config_classes (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	description TEXT);
+
+CREATE TABLE IF NOT EXISTS tng.config_properties (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	description TEXT
+	)
+
+
+
+
