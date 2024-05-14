2118e14a60ace68af0ca117fcc9e58e7479900e7
Stefan <stefan.eichert@univie.ac.at>
2024-05-14 14:52:52 +0200

    add sql statements

--- install/about.sql
+++ install/about.sql
@@ -1,57 +1,84 @@
 DROP SCHEMA IF EXISTS tng CASCADE;
 CREATE SCHEMA IF NOT EXISTS tng;
 
-CREATE TABLE IF NOT EXISTS tng.config (
-    id SERIAL PRIMARY KEY,
-  	name TEXT,
-    description TEXT,
-   	address TEXT,
+CREATE TABLE IF NOT EXISTS tng.config
+(
+    id           SERIAL PRIMARY KEY,
+    name         TEXT,
+    description  TEXT,
+    address      TEXT,
     config_class INT,
-    email TEXT,
-    orcid_id TEXT,
-    image TEXT,
-    website TEXT,
-    language TEXT DEFAULT 'en'
+    email        TEXT,
+    orcid_id     TEXT,
+    image        TEXT,
+    website      TEXT,
+    language     TEXT DEFAULT 'en'
 );
 
-CREATE TABLE IF NOT EXISTS tng.links (
- 	id SERIAL PRIMARY KEY,
-	domain_id INT,
-	range_id INT,
-	property INT
-	);
+CREATE TABLE IF NOT EXISTS tng.links
+(
+    id        SERIAL PRIMARY KEY,
+    domain_id INT,
+    range_id  INT,
+    property  INT,
+    attribute INT
+);
 
-CREATE TABLE IF NOT EXISTS tng.config_classes (
-	id SERIAL PRIMARY KEY,
-	name TEXT,
-	description TEXT);
+CREATE TABLE IF NOT EXISTS tng.config_classes
+(
+    id          SERIAL PRIMARY KEY,
+    name        TEXT,
+    description TEXT
+);
 
-CREATE TABLE IF NOT EXISTS tng.config_properties (
-	id SERIAL PRIMARY KEY,
-	name TEXT,
-	description TEXT);
+CREATE TABLE IF NOT EXISTS tng.config_properties
+(
+    id          SERIAL PRIMARY KEY,
+    name        TEXT,
+    description TEXT
+);
 
-
-ALTER TABLE tng.config ADD CONSTRAINT config_config_classes_fk FOREIGN KEY (config_class) REFERENCES tng.config_classes(id);
-ALTER TABLE tng.links ADD CONSTRAINT links_config_properties_fk FOREIGN KEY (property) REFERENCES tng.config_properties(id);
-ALTER TABLE tng.links ADD CONSTRAINT links_config_fk_domain FOREIGN KEY (domain_id) REFERENCES tng.config(id);
-ALTER TABLE tng.links ADD CONSTRAINT links_config_fk_range FOREIGN KEY (range_id) REFERENCES tng.config(id);
-ALTER TABLE tng.links ADD CONSTRAINT links_config_fk_role FOREIGN KEY (role) REFERENCES tng.config(id);
+ALTER TABLE tng.config
+    ADD CONSTRAINT config_config_classes_fk FOREIGN KEY (config_class) REFERENCES tng.config_classes (id);
+ALTER TABLE tng.links
+    ADD CONSTRAINT links_config_properties_fk FOREIGN KEY (property) REFERENCES tng.config_properties (id);
+ALTER TABLE tng.links
+    ADD CONSTRAINT links_config_fk_domain FOREIGN KEY (domain_id) REFERENCES tng.config (id);
+ALTER TABLE tng.links
+    ADD CONSTRAINT links_config_fk_range FOREIGN KEY (range_id) REFERENCES tng.config (id);
+ALTER TABLE tng.links
+    ADD CONSTRAINT links_config_fk_role FOREIGN KEY (attribute) REFERENCES tng.config (id);
 
 CREATE OR REPLACE FUNCTION tng.delete_links_on_config_delete()
- RETURNS trigger
- LANGUAGE plpgsql
-AS $function$
+    RETURNS trigger
+    LANGUAGE plpgsql
+AS
+$function$
 BEGIN
     DELETE FROM tng.links WHERE domain_id = OLD.id OR range_id = OLD.id;
     RETURN OLD;
 END;
 $function$;
 
-create trigger delete_links_trigger before
-delete
-    on
-    tng.config for each row execute function tng.delete_links_on_config_delete();
+CREATE TRIGGER delete_links_trigger
+    BEFORE DELETE
+    ON tng.config
+    FOR EACH ROW
+EXECUTE FUNCTION tng.delete_links_on_config_delete();
+
+INSERT INTO tng.config_classes (name) VALUES ('main_project');
+INSERT INTO tng.config_classes (name) VALUES ('project');
+INSERT INTO tng.config_classes (name) VALUES ('person');
+INSERT INTO tng.config_classes (name) VALUES ('role');
+INSERT INTO tng.config_classes (name) VALUES ('institution');
+INSERT INTO tng.config_classes (name) VALUES ('language_code');
+
+INSERT INTO tng.config_properties (name) VALUES ('has member');
+INSERT INTO tng.config_properties (name) VALUES ('has affiliation');
+INSERT INTO tng.config_properties (name) VALUES ('has translation');
+
+INSERT INTO tng.config (name, config_class, description, address, email, website) VALUES ('Max Mustermann', (SELECT id from tng.config_classes WHERE name = 'person'), NULL, NULL, 'example@example.exampe', 'https://example.exampe');
+
 
 
 
