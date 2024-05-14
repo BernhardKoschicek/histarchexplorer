e7d6bb69337bc2ba0fe0a6e81b2055038f49d204
lisaldrian <lis.aldrian@gmail.com>
2024-05-14 14:34:37 +0200

    foreign keys, delete links, when deleting row in config function/trigger  -

--- install/about.sql
+++ install/about.sql
@@ -29,8 +29,29 @@ CREATE TABLE IF NOT EXISTS tng.config_classes (
 CREATE TABLE IF NOT EXISTS tng.config_properties (
 	id SERIAL PRIMARY KEY,
 	name TEXT,
-	description TEXT
-	)
+	description TEXT);
+
+
+ALTER TABLE tng.config ADD CONSTRAINT config_config_classes_fk FOREIGN KEY (config_class) REFERENCES tng.config_classes(id);
+ALTER TABLE tng.links ADD CONSTRAINT links_config_properties_fk FOREIGN KEY (property) REFERENCES tng.config_properties(id);
+ALTER TABLE tng.links ADD CONSTRAINT links_config_fk_domain FOREIGN KEY (domain_id) REFERENCES tng.config(id);
+ALTER TABLE tng.links ADD CONSTRAINT links_config_fk_range FOREIGN KEY (range_id) REFERENCES tng.config(id);
+ALTER TABLE tng.links ADD CONSTRAINT links_config_fk_role FOREIGN KEY (role) REFERENCES tng.config(id);
+
+CREATE OR REPLACE FUNCTION tng.delete_links_on_config_delete()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+    DELETE FROM tng.links WHERE domain_id = OLD.id OR range_id = OLD.id;
+    RETURN OLD;
+END;
+$function$;
+
+create trigger delete_links_trigger before
+delete
+    on
+    tng.config for each row execute function tng.delete_links_on_config_delete();
 
 
 
