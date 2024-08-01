f195a169e3ea6092e8bec3e241d8a388d76869ef
lisaldrian <lis.aldrian@gmail.com>
2024-08-01 14:44:58 +0200

    entity cards custom to bootstrap

--- histarchexplorer/views/entities.py
+++ histarchexplorer/views/entities.py
@@ -2,6 +2,7 @@ from flask import render_template, g
 
 from histarchexplorer import app
 
+from histarchexplorer.utils import cerberos
 
 @app.route('/entities')
 def entities() -> str:
@@ -16,7 +17,8 @@ def entities() -> str:
     # list of tuples to dictionary (entities_dict); keys = entities; values = associated IDs.
     entities_dict = {entity[0]: entity[1] for entity in entities}
 
-    view_classes = app.config['VIEW_CLASSES']
+    view_classes = cerberos.gatekeeper()
+    print(view_classes)
 
 
     g.cursor.execute('''
@@ -26,4 +28,8 @@ def entities() -> str:
     for shown_entity in shown_entities:
         print(shown_entity)
 
-    return render_template('entities.html', entities=entities_dict, view_classes=view_classes)
+    cerberos.gatekeeper()
+
+    return render_template('entities.html', entities=entities_dict, view_classes=view_classes, viewcount=len(view_classes.keys()))
+
+
