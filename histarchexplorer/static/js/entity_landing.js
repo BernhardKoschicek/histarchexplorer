e7d0c2f776083e7e9ad4bfea94c20ce313b2125a
lisaldrian <lis.aldrian@gmail.com>
2024-09-11 15:05:32 +0200

    map; layout grid start

--- histarchexplorer/static/js/entity_landing.js
+++ histarchexplorer/static/js/entity_landing.js
@@ -12,7 +12,7 @@ window.onload = function () {
 };
 
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('muuri-map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
