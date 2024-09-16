39cd3a8d26f37ca860d383980648019c5de3f9a3
lisaldrian <lis.aldrian@gmail.com>
2024-09-16 10:13:40 +0200

    map layout

--- histarchexplorer/views/landing.py
+++ histarchexplorer/views/landing.py
@@ -14,4 +14,13 @@ def landing(id_: int) -> str:
     print(entity.end)
     print(entity.relations)
     print(entity.relation_class)
-    return render_template('landing.html', entity=entity, relations=entity.relations)
+
+    main_image = None
+    if entity.depictions:
+        main_image = next((depiction for depiction in entity.depictions if depiction.is_main), None)
+
+
+    if not main_image and entity.depictions:
+        main_image = random.choice(entity.depictions)
+
+    return render_template('landing.html', entity=entity, relations=entity.relations, main_image=main_image)
