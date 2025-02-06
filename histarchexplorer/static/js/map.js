9631bf469fdf59819b585f64d6b25b88d1b1258c
Stefan <stefan.eichert@univie.ac.at>
2025-02-06 13:49:34 +0100

    maptilertest

--- histarchexplorer/static/js/map.js
+++ histarchexplorer/static/js/map.js
@@ -1,66 +1,36 @@
-const map = (window.map = new maplibregl.Map({
-    container: 'mymap',
-    zoom: 12,
-    center: [11.39085, 47.27574],
+ maptilersdk.config.apiKey = 'E7Jrgaazm79UlTuEI5f5';
+
+const map = new maptilersdk.Map({
+    container: 'mymap', // container id
+    style: "dataviz",
+    center: [8.94738, 45.97812], // starting position [lng, lat]
+    zoom: 14, // starting zoom
+    terrain: true,
+    terrainControl: true,
     pitch: 70,
-    hash: true,
-    style: {
-        version: 8,
-        sources: {
-            osm: {
-                type: 'raster',
-                tiles: ['https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'],
-                tileSize: 256,
-                attribution: '&copy; OpenStreetMap Contributors',
-                maxzoom: 19
-            },
-            // Use a different source for terrain and hillshade layers, to improve render quality
-            terrainSource: {
-                type: 'raster-dem',
-                url: 'https://demotiles.maplibre.org/terrain-tiles/tiles.json',
-                tileSize: 256
-            },
-            hillshadeSource: {
-                type: 'raster-dem',
-                url: 'https://demotiles.maplibre.org/terrain-tiles/tiles.json',
-                tileSize: 256
-            }
-        },
-        layers: [
-            {
-                id: 'osm',
-                type: 'raster',
-                source: 'osm'
-            },
-            {
-                id: 'hills',
-                type: 'hillshade',
-                source: 'hillshadeSource',
-                layout: {visibility: 'visible'},
-                paint: {'hillshade-shadow-color': '#473B24'}
-            }
+    bearing: -100.86,
+    maxPitch: 85,
+    maxZoom: 25
+  });
+
+map.on('load', () => {
+    map.addSource('hillshade', {
+        type: 'raster',
+        tiles: [
+            'https://api.maptiler.com/tiles/hillshades/{z}/{x}/{y}.png?key=E7Jrgaazm79UlTuEI5f5'
         ],
-        terrain: {
-            source: 'terrainSource',
-            exaggeration: 1
-        },
-        sky: {}
-    },
-    maxZoom: 18,
-    maxPitch: 85
-}));
+        tileSize: 256,
+        maxzoom: 15
+    });
 
-map.addControl(
-    new maplibregl.NavigationControl({
-        visualizePitch: true,
-        showZoom: true,
-        showCompass: true
-    })
-);
+    map.addLayer({
+        id: 'hillshade-layer',
+        type: 'raster',
+        source: 'hillshade',
+        layout: {},
+        paint: {
+            'raster-opacity': 0.5 // Adjust transparency (0 = invisible, 1 = full)
+        }
+    });
+});
 
-map.addControl(
-    new maplibregl.TerrainControl({
-        source: 'terrainSource',
-        exaggeration: 1
-    })
-);
