876884ee66f832e0684c76db8c9df2cf71a44196
cureboyxxx <eliasgruenbacher@gmail.com>
2025-05-23 14:15:56 +0200

    move sql statments from code to new file in database folder

--- histarchexplorer/views/about.py
+++ histarchexplorer/views/about.py
@@ -1,6 +1,7 @@
 import json
 from flask import render_template, g
 from histarchexplorer import app
+from histarchexplorer.database.about import *
 from histarchexplorer.utils import helpers
 
 
@@ -31,15 +32,7 @@ def about() -> str:
         return object
 
     def build_connections(id):
-        g.cursor.execute('''
-            SELECT p.name AS property, p.id AS property_id, c.name AS target, d.name AS role, l.sortorder, c.id AS target_id
-            FROM tng.links l
-            JOIN tng.config c ON l.range_id = c.id
-            JOIN tng.config_properties p ON l.property = p.id
-            LEFT JOIN tng.config d ON l.attribute = d.id
-            WHERE domain_id = %s
-            ORDER BY property, sortorder
-        ''', (id,))
+        g.cursor.execute(build_connections_sql, (id,))
         rows = g.cursor.fetchall()
 
         result = {'properties': []}
@@ -80,18 +73,8 @@ def about() -> str:
         'imprint': (helpers.get_translation(project_result[3]))['label']
     }
 
-    institutions_sql = """
-        SELECT c.name, c.address, c.website, c.image, role.name AS role
-        FROM tng.links l
-        JOIN tng.config c ON l.range_id = c.id
-        LEFT JOIN tng.config role ON l.attribute = role.id AND role.config_class = '3' --role name & config class
-        WHERE l.domain_id = (SELECT id FROM tng.config WHERE config_class = '5') -- only links concerning main-project
-        AND c.config_class = '4'
-        ORDER BY l.sortorder, l.id;
-    """
 
-    g.cursor.execute(institutions_sql)
-    institutions_result = g.cursor.fetchall()
+    institutions_result = get_institutions()
 
 
     institutions = []
@@ -105,22 +88,8 @@ def about() -> str:
         })
 
 
-    persons_sql = """
-SELECT p.name, p.image, b.name AS role, a.name AS affiliation, COALESCE(a.website, '') AS website, COALESCE(p.email, '') AS email
-FROM tng.links l
-JOIN tng.config p ON l.range_id = p.id
-JOIN tng.config_properties cp ON l.property = cp.id
-LEFT JOIN tng.config b ON l.attribute = b.id AND b.config_class = '3'
-LEFT JOIN tng.links la ON la.domain_id = p.id AND la.property = 2
-LEFT JOIN tng.config a ON la.range_id = a.id
-WHERE l.domain_id = (SELECT id FROM tng.config WHERE config_class = '5')
-AND p.config_class = '2'
-AND cp.id = 4
-ORDER BY l.sortorder, l.id;
-    """
 
-    g.cursor.execute(persons_sql)
-    persons_result = g.cursor.fetchall()
+    persons_result = get_persons()
 
 
     persons = {}
