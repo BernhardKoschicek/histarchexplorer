02cd3bee6a756026771cd5784d1f66e0e2348d34
lisaldrian <lis.aldrian@gmail.com>
2025-07-14 14:44:38 +0200

    3d model spinner

--- histarchexplorer/static/js/overview.js
+++ histarchexplorer/static/js/overview.js
@@ -15,20 +15,33 @@ window.onload = function () {
 
 // Wait for DOM and model-viewer
 window.addEventListener('DOMContentLoaded', () => {
-  if (customElements.get('model-viewer')) {
-    initMuuri();
-  } else {
-    customElements.whenDefined('model-viewer').then(() => {
-      initMuuri();
-    });
-  }
+    if (customElements.get('model-viewer')) {
+        initMuuri();
+    } else {
+        customElements.whenDefined('model-viewer').then(() => {
+            initMuuri();
+        });
+    }
 });
 
 let resizeTimeout;
 
 window.addEventListener('resize', () => {
-  clearTimeout(resizeTimeout);
-  resizeTimeout = setTimeout(() => {
-    grid.refreshItems().layout();
-  }, 300);
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(() => {
+        grid.refreshItems().layout();
+    }, 300);
+});
+
+console.log("overview.js geladen");
+//3d Model Spinner removal when 3dmodel loaded
+document.querySelectorAll('model-viewer').forEach(model => {
+    model.addEventListener('poster-dismissed', () => {
+        console.log('Poster dismissed, Modell geladen.');
+        const wrapper = model.closest('.model-wrapper');
+        if (wrapper) {
+            const spinner = wrapper.querySelector('.spinner');
+            if (spinner) spinner.style.display = 'none';
+        }
+    });
 });
