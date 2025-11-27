5e1e97e711cfbeee4fef5ba358bcd38e13f77dc0
Bernhard Koschicek-Krombholz <b.koschicek@gmail.com>
2025-11-27 16:29:10 +0100

    styled the about site

--- histarchexplorer/views/about.py
+++ histarchexplorer/views/about.py
@@ -23,9 +23,36 @@ def about(id_: Optional[int] = None):
             if p.id != id_:
                 project_choices.append(p)
 
+    people_map = {}
+    institutions_map = {}
+
+    for link in active.links:
+        target = next((
+            e for e in g.config_entities if e.id == link.end_id), None)
+        if not target:
+            continue
+
+        role = None
+        if link.role and link.role['display']:
+            role = link.role['display']['label']
+
+        if target.class_name == "person":
+            if target.id not in people_map:
+                people_map[target.id] = {"entity": target, "roles": []}
+            if role:
+                people_map[target.id]["roles"].append(role)
+
+        elif target.class_name == "institution":
+            if target.id not in institutions_map:
+                institutions_map[target.id] = {"entity": target, "roles": []}
+            if role:
+                institutions_map[target.id]["roles"].append(role)
+
     return render_template(
         "about.html",
         active=active,
         main_project=main_project,
         sub_projects=project_choices or sub_projects,
-        config_entities_mapped=config_entities_mapped)
+        config_entities_mapped=config_entities_mapped,
+        people=list(people_map.values()),
+        institutions=list(institutions_map.values()))
